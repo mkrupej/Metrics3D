10516e083fab6e0b590eedc180d6ccb7bd965461
michal <michalkrupej@gmail.com>
2018-06-03 12:13:24 +0200

    downloader fixed

--- loader/BasePairLoader.py
+++ loader/BasePairLoader.py
@@ -11,21 +11,27 @@ class BasePairLoader(object):
 
     def retrieve_base_pair(self, pdb_id_list):
         for pdb_id in pdb_id_list:
-            save_path = "base_pair/{}".format(pdb_id)
+            save_path = "../base_pair/{}".format(pdb_id)
             download_path = self.BASE_PAIR_CALCULATOR.format(pdb_id)
 
-            self.download_file(save_path, download_path)
+            self.download_file(download_path, save_path)
 
     def retrieve_stacking(self, pdb_id_list):
         for pdb_id in pdb_id_list:
-            save_path = "base_stacking/{}".format(pdb_id)
+            save_path = "../base_stacking/{}".format(pdb_id)
             download_path = self.BASE_STACKING_CALCULATOR.format(pdb_id)
 
-            self.download_file(save_path, download_path)
+            self.download_file(download_path, save_path)
 
     @staticmethod
-    def download_file(save_path, download_path):
+    def download_file(download_path, save_path):
         if not os.path.exists(save_path):
-            wget.download(save_path, download_path)
+            wget.download(download_path, save_path)
         else:
             logging.info('File exists: {}'.format(save_path))
+
+
+a = BasePairLoader()
+
+a.retrieve_stacking(["1EHZ", "1EVV"])
+a.retrieve_base_pair(["1EHZ", "1EVV"])
