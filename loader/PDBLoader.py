8e9039bdb2d42380c74b791da9e8ff3f90e6540e
michal <michalkrupej@gmail.com>
2018-06-10 17:30:04 +0200

    more docs

--- loader/PDBLoader.py
+++ loader/PDBLoader.py
@@ -21,12 +21,27 @@ class PDBLoader(object):
         return self.parser.get_structure('pdb_id', file_path)
 
     def get_residue(self, pdb_path):
+        """
+        Get list of residues in structure for given PDB path
+        :param pdb_path: path to pdb file
+        :return: List of residues in structure
+        """
         return list(self.parse_structure_by_file(pdb_path).get_residues())
 
     def get_atoms(self, pdb_path):
+        """
+        Get list of atoms in structure for given PDB path
+        :param pdb_path: path to pdb file
+        :return: List of atoms in structure
+        """
         return list(self.parse_structure_by_file(pdb_path).get_atoms())
 
     def get_length(self, pdb_id):
+        """
+        Get length of given pdb structure
+        :param pdb_id:
+        :return: length of pdb structure
+        """
         return len(self.get_residue(pdb_id))
 
     def get_residue_as_map(self, pdb_path):
