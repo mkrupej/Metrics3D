7a7c6fa1408f58acd06678160eadc61515ffafe0
michal <michalkrupej@gmail.com>
2018-06-10 17:24:29 +0200

    docs added

--- loader/BasePairLoader.py
+++ loader/BasePairLoader.py
@@ -1,3 +1,7 @@
+"""
+BasePairLoader module - responsible for downloading and extracting base pair
+ and stacking alignment for given pdb files.
+"""
 import wget
 import os
 from loader.BasePairParser import *
@@ -12,9 +16,20 @@ class BasePairLoader(object):
     SAVE_PATH_STACKING = "base_stacking/{}"
 
     def __init__(self):
+        """
+        Initialize - save path for base pairs and base stackings pairs given
+        :param self:
+        :return:
+        """
         self.parser = BasePairParser(self.SAVE_PATH_PAIR, self.SAVE_PATH_STACKING)
 
     def retrieve_base_pair(self, pdb_id_list):
+        """
+        Retrieves base pair alignment from server for given pdb files list.
+        Files are saved in SAVE_PATH PAIR path
+        :param pdb_id_list:
+        :return: -
+        """
         if not os.path.exists(self.SAVE_PATH_PAIR.format("")):
             os.makedirs("base_pair")
 
@@ -25,6 +40,13 @@ class BasePairLoader(object):
             self.download_file(download_path, save_path)
 
     def retrieve_stacking(self, pdb_id_list):
+        """
+        Retrieves stacking alignment from server for given pdb files list.
+        Files are saved in SAVE_PATH_STACKING path
+        :param self:
+        :param pdb_id_list:
+        :return:
+        """
         if not os.path.exists(self.SAVE_PATH_STACKING.format("")):
             os.makedirs("base_stacking")
 
@@ -36,12 +58,26 @@ class BasePairLoader(object):
 
     @staticmethod
     def download_file(download_path, save_path):
+        """
+        Downloads given file from server into given save path.
+        If file already exists then skip
+        :param download_path:
+        :param save_path:
+        :return: -
+        """
         if not os.path.exists(save_path):
             wget.download(download_path, save_path)
         else:
             print('File exists: {}'.format(save_path))
 
     def get_all_pairs(self, first_pdb_id, second_pdb_id):
+        """
+        Get ALL base pairs alignment from given two pdb ids
+        :param self:
+        :param first_pdb_id:
+        :param second_pdb_id:
+        :return: two lists containing base pairs for each pdb_id. Every pair is stored in dictionary.
+        """
         self.retrieve_base_pair([first_pdb_id, second_pdb_id])
         first_result = self.parser.extract_pair(first_pdb_id).get_all_pairs()
         print(len(first_result))
@@ -52,24 +88,52 @@ class BasePairLoader(object):
         return first_result, second_result
 
     def get_wc(self, first_pdb_id, second_pdb_id):
+        """
+        Get WATSON-CRICK base pairs alignment from given two pdb ids
+        :param self:
+        :param first_pdb_id:
+        :param second_pdb_id:
+        :return: two lists containing base pairs for each pdb_id. Every pair is stored in dictionary.
+        """
         self.retrieve_base_pair([first_pdb_id, second_pdb_id])
         first_result = self.parser.extract_pair(first_pdb_id).get_wc_pairs()
         second_result = self.parser.extract_pair(second_pdb_id).get_wc_pairs()
         return first_result, second_result
 
     def get_nwc(self, first_pdb_id, second_pdb_id):
+        """
+        Get ALL base pairs alignment except WATSON-CRICK from given two pdb ids
+        :param self:
+        :param first_pdb_id:
+        :param second_pdb_id:
+        :return: two lists containing base pairs for each pdb_id. Every pair is stored in dictionary.
+        """
         self.retrieve_base_pair([first_pdb_id, second_pdb_id])
         first_result = self.parser.extract_pair(first_pdb_id).get_nwc_pairs()
         second_result = self.parser.extract_pair(second_pdb_id).get_nwc_pairs()
         return first_result, second_result
 
     def get_stacking(self, first_pdb_id, second_pdb_id):
+        """
+         Get ALL stacking pairs alignment from given two pdb ids
+         :param self:
+         :param first_pdb_id:
+         :param second_pdb_id:
+         :return: two lists containing base pairs for each pdb_id. Every pair is stored in dictionary.
+         """
         self.retrieve_stacking([first_pdb_id, second_pdb_id])
         first_result = self.parser.extract_stacking(first_pdb_id).get_stacking()
         second_result = self.parser.extract_stacking(second_pdb_id).get_stacking()
         return first_result, second_result
 
     def get_all(self, first_pdb_id, second_pdb_id):
+        """
+         Get ALL base pairs and stacking alignment from given two pdb ids
+         :param self:
+         :param first_pdb_id:
+         :param second_pdb_id:
+         :return: two lists containing base pairs for each pdb_id. Every pair is stored in dictionary.
+         """
         self.retrieve_base_pair([first_pdb_id, second_pdb_id])
         self.retrieve_stacking([first_pdb_id, second_pdb_id])
         first_result_stacking = self.parser.extract_stacking(first_pdb_id).get_stacking()
--- loader/BasePairParser.py
+++ loader/BasePairParser.py
@@ -1,3 +1,6 @@
+"""
+Module responsible for parsing file containing base pair / base stacking alignment
+"""
 import copy
 import csv
 
@@ -5,20 +8,39 @@ import csv
 class BasePairParser(object):
 
     def __init__(self, base_pair_path, base_stacking_path):
+        """
+        Initialize - path to downloaded base_pair / base_stacking files
+        :param self:
+        :return:
+        """
         self.PAIR = base_pair_path
         self.STACKING = base_stacking_path
 
     def extract_stacking(self, pdb_id):
+        """
+        Returns extracted stacking alignment as list for given pdb_id
+        :param pdb_id:
+        :return: stacking alignment as BasePair objcets
+        """
         file = self.STACKING.format(pdb_id)
         return BaseStacking(self.extract_alignment(file=file))
 
     def extract_pair(self, pdb_id):
+        """
+        Returns extracted base pair alignment as list for given pdb_id
+        :param pdb_id:
+        :return: base pair alignment BasePair object
+        """
         file = self.PAIR.format(pdb_id)
         return BasePair(self.extract_alignment(file=file))
 
     @staticmethod
     def extract_alignment(file):
-
+        """
+        Extracts alignment from given file. Saves each pair in dictionary called pair
+        :param file: path to file which is processed
+        :return: List of pair dictionaries
+        """
         alignment = []
 
         pair = {'a_model': None, 'a_chain': None, 'a_compound': None, 'a_number': None,
@@ -26,7 +48,7 @@ class BasePairParser(object):
                 'interaction': None}
 
         with open(file) as csv_file:
-                line_reader = csv.reader(csv_file, delimiter='|', quotechar= " ")
+                line_reader = csv.reader(csv_file, delimiter='|', quotechar=" ")
 
                 for line in line_reader:
                     pair['a_model'] = line[1]
@@ -48,23 +70,47 @@ class BasePair(object):
 
     watson_crick = ["cWW", "tWW", "cWH", "tWH", "cWS", "tWS"]
 
-    def __init__(self,alignment):
+    def __init__(self, alignment):
+        """
+        Initialized with given BasePairParser alignment
+        :param alignment:
+        """
         self.alignment = alignment
 
     def get_all_pairs(self):
+        """
+        Get all base pairs
+        :return: returns all base pairs as list of dictionaries
+        """
         return self.alignment
 
     def get_wc_pairs(self):
+        """
+        Get Watson Crick pairs
+        :return: returns all watson crick pairs filtered from the whole alignment as a list of dictionaries
+        """
         return [pair for pair in self.alignment if pair['interaction'] in self.watson_crick]
 
     def get_nwc_pairs(self):
+        """
+        Get non Watson Crick pairs
+        :return: returns all non watson crick pairs filtered from the whole alignment as a list of dictionaries
+        """
         return [pair for pair in self.alignment if pair not in self.get_wc_pairs()]
 
 
 class BaseStacking(object):
 
-    def __init__(self,alignment):
+    def __init__(self, alignment):
+        """
+        Initialized with given BasePairParser alignment
+        :param alignment:
+        """
         self.alignment = alignment
 
     def get_stacking(self):
+        """
+        Get non base stacking alignment
+        :return: returns all stacking alignment as a list of dictionaries
+        """
         return self.alignment
